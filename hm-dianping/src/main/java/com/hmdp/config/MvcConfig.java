6e27f2e45efcd2354705b7da602a3be941a5eb6b
nanquan <1124719347@qq.com>
2025-05-10 16:08:57 +0800

    已实现短信验证码登录和登录状态刷新功能

---
+++ hm-dianping/src/main/java/com/hmdp/config/MvcConfig.java
@@ -0,0 +1,43 @@
+package com.hmdp.config;
+
+import com.hmdp.utils.LoginInterceptor;
+import com.hmdp.utils.RefreshTokenInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+/**
+ * @author Administrator
+ * @date 2025/5/1
+ */
+
+/**
+ * 拦截器配置类
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //登录拦截器
+        registry.addInterceptor(new LoginInterceptor())
+                .excludePathPatterns(
+                        "/user/code",
+                        "/user/login",
+
+                        "/blog/hot",
+                        "shop/**",
+                        "/shop-type/**",
+                        "/upload/**",
+                        "/voucher"
+                        ).order(1);         // .order(1)表示优先级，数字越小，优先级越高
+        //token刷新的拦截器
+        registry.addInterceptor(new RefreshTokenInterceptor(stringRedisTemplate)).addPathPatterns("/**").order(0);   // "/**"表示匹配所有路径
+    }
+}
---
+++ hm-dianping/src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,43 @@
+package com.hmdp.utils;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
+import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.User;
+import com.hmdp.entity.UserInfo;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Administrator
+ * @date 2025/4/10
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 1.判断是否需要拦截（threadlocal中是否有用户）
+        if (UserHolder.getUser() == null){
+
+            //不存在，拦截，设置状态码
+            response.setStatus(401);
+            return false;
+        }
+        // 存在，放行
+        System.out.println("当前用户：" +UserHolder.getUser());
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        //移除用户
+        UserHolder.removeUser();
+    }
+
+}
