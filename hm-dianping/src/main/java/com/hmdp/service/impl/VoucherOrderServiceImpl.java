5de2163e4b39f5073bafb7238d4ea0b8753a9a61
nanquan <1124719347@qq.com>
2025-05-14 00:49:36 +0800

    使用乐观锁CAS解决了超卖问题

--- hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -51,7 +51,11 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         }
 
         // 5.扣减库存
-        Boolean success = seckillVoucherService.update().setSql("stock = stock - 1").eq("voucher_id",voucherId).update();
+        Boolean success = seckillVoucherService.update()
+                .setSql("stock = stock - 1")            // set stock = stock - 1
+                .eq("voucher_id",voucherId)
+                .gt("stock", 0) // where id = ? and stock = ?
+                .update();
         if (!success){
             return Result.fail("库存不足");
         }
